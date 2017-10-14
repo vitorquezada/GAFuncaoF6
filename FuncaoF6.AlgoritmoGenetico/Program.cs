b2e465e1ff58a09a68a38bf84fdcfa5485ccc427
Vitor Quezada <vitorquezada@gmail.com>
2017-10-14 10:10:24 -0300

    Adicionar arquivos de projeto.

---
+++ FuncaoF6.AlgoritmoGenetico/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncaoF6.AlgoritmoGenetico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
